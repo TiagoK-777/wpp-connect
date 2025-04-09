44b916f41c39dfc54c0ce001dc2e1837578da758
TiagoK-777 <tiagokruger404@gmail.com>
2025-04-09 19:56:23 -0300

    fix

--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ if [ -f /data/options.json ]; then
   export FRONTEND=$(jq -r '.FRONTEND // ""' /data/options.json)
   export START_ALL_SESSION=$(jq -r '.START_ALL_SESSION // ""' /data/options.json)
   export WEBHOOK_URL=$(jq -r '.WEBHOOK_URL // ""' /data/options.json)
+  export NO_WEBHOOK_READMESSAGE=$(jq -r '.NO_WEBHOOK_READMESSAGE // ""' /data/options.json)
+  export NO_WEBHOOK_LISTENACKS=$(jq -r '.NO_WEBHOOK_LISTENACKS // ""' /data/options.json)
+  export NO_WEBHOOK_ONPRESENCECHANGED=$(jq -r '.NO_WEBHOOK_ONPRESENCECHANGED // ""' /data/options.json)
+  export NO_WEBHOOK_ONPARTICIPANTSCHANGED=$(jq -r '.NO_WEBHOOK_ONPARTICIPANTSCHANGED // ""' /data/options.json)
 fi
 
 mkdir -p /data/tokens /data/userDataDir
@@ -18,5 +22,9 @@ CMD="node bin/wppserver.js"
 [ "$FRONTEND" = "true" ] && CMD="$CMD --frontend"
 [ "$START_ALL_SESSION" = "true" ] && CMD="$CMD --startAllSession"
 [ -n "$WEBHOOK_URL" ] && CMD="$CMD --webhook-url $WEBHOOK_URL"
+[ -n "$NO_WEBHOOK_READMESSAGE" = "true"  ] && CMD="$CMD --no-webhook-readMessage $NO_WEBHOOK_READMESSAGE"
+[ -n "$NO_WEBHOOK_LISTENACKS" = "true"  ] && CMD="$CMD --no-webhook-listenAcks $NO_WEBHOOK_LISTENACKS"
+[ -n "$NO_WEBHOOK_ONPRESENCECHANGED" = "true"  ] && CMD="$CMD --no-webhook-onPresenceChanged $NO_WEBHOOK_ONPRESENCECHANGED"
+[ -n "$NO_WEBHOOK_ONPARTICIPANTSCHANGED" = "true"  ] && CMD="$CMD --no-webhook-onParticipantsChanged $NO_WEBHOOK_ONPARTICIPANTSCHANGED"
 
 exec $CMD 2>&1 | iconv -f ISO-8859-1 -t UTF-8
