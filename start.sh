7a0bdcba88a4c2dbfc3a98b2774037adbb85c61f
TiagoK-777 <tiagokruger404@gmail.com>
2025-04-16 21:15:19 -0300

    atualizar server para v1.37.0

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ if [ -f /data/options.json ]; then
   export START_ALL_SESSION=$(jq -r '.START_ALL_SESSION // ""' /data/options.json)
   export WEBHOOK_URL=$(jq -r '.WEBHOOK_URL // ""' /data/options.json)
   export NO_WEBHOOK_READMESSAGE=$(jq -r '.NO_WEBHOOK_READMESSAGE // ""' /data/options.json)
+  export WEBHOOK_READMESSAGE=$(jq -r '.WEBHOOK_READMESSAGE // ""' /data/options.json)
   export NO_WEBHOOK_LISTENACKS=$(jq -r '.NO_WEBHOOK_LISTENACKS // ""' /data/options.json)
   export NO_WEBHOOK_ONPRESENCECHANGED=$(jq -r '.NO_WEBHOOK_ONPRESENCECHANGED // ""' /data/options.json)
   export NO_WEBHOOK_ONPARTICIPANTSCHANGED=$(jq -r '.NO_WEBHOOK_ONPARTICIPANTSCHANGED // ""' /data/options.json)
@@ -23,6 +24,7 @@ CMD="node bin/wppserver.js"
 [ "$START_ALL_SESSION" = "true" ] && CMD="$CMD --startAllSession"
 [ -n "$WEBHOOK_URL" ] && CMD="$CMD --webhook-url $WEBHOOK_URL"
 [ "$NO_WEBHOOK_READMESSAGE" = "true" ] && CMD="$CMD --no-webhook-readMessage"
+[ "$WEBHOOK_READMESSAGE" = "true" ] && CMD="$CMD --webhook-readMessage"
 [ "$NO_WEBHOOK_LISTENACKS" = "true" ] && CMD="$CMD --no-webhook-listenAcks"
 [ "$NO_WEBHOOK_ONPRESENCECHANGED" = "true" ] && CMD="$CMD --no-webhook-onPresenceChanged"
 [ "$NO_WEBHOOK_ONPARTICIPANTSCHANGED" = "true" ] && CMD="$CMD --no-webhook-onParticipantsChanged"
